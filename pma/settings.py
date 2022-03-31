939e26561083f78a3f98e527f6482a38e8a19d52
massimiliano <maxvilla970@gmail.com>
2022-03-31 14:11:07 +0200

    Modificato settings.py

--- pma/settings.py
+++ pma/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
